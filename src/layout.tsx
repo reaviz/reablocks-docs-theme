23e2cdd8447061e922b080e35e755e7d59b9de9c
Serhii.Tsybulskyi <c4r0n0s@gmail.com>
2025-06-26 10:35:47 +0300

    upgrade theme for Nextra4

---
+++ src/layout.tsx
@@ -0,0 +1,132 @@
+/* eslint sort-keys: error */
+import { ThemeProvider } from 'next-themes'
+import { Search, SkipNavLink } from 'nextra/components'
+import { element, reactNode } from 'nextra/schemas'
+import type { FC, ReactNode } from 'react'
+import { Fragment } from 'react'
+import { z } from 'zod'
+import { fromZodError } from 'zod-validation-error'
+import { LastUpdated } from './components/last-updated'
+import { MobileNav } from './components/sidebar'
+import { ConfigProvider, ThemeConfigProvider } from './stores'
+
+const attributeSchema = z.custom<'class' | `data-${string}`>(
+  value => value === 'class' || value.startsWith('data-')
+)
+
+const theme = z.strictObject({
+  banner: reactNode,
+  darkMode: z.boolean().default(true),
+  docsRepositoryBase: z
+    .string()
+    .startsWith('https://')
+    .default('https://github.com/goodcodeus'),
+  editLink: reactNode.default('Edit this page'),
+  feedback: z
+    .strictObject({
+      content: reactNode.default('Question? Give us feedback'),
+      labels: z.string().default('feedback')
+    })
+    .default({}),
+  footer: reactNode,
+  i18n: z
+    .array(
+      z.strictObject({
+        locale: z.string(),
+        name: z.string()
+      })
+    )
+    .default([]),
+  lastUpdated: element
+    .default(<LastUpdated />)
+    .refine(el => el.type !== Fragment && typeof el.type !== 'string', {
+      message: `\`Layout#lastUpdated\` must be a \`<LastUpdated />\` component:
+
+\`\`\`js
+import { Layout, LastUpdated } from 'nextra-theme-docs'
+
+<Layout
+  lastUpdated={<LastUpdated locale="YOUR_LOCALE">YOUR_CONTENT</LastUpdated>}
+>
+  {children}
+</Layout>
+\`\`\`
+`
+    }),
+  navbar: reactNode,
+  navigation: z
+    .union([
+      z.boolean(),
+      z.strictObject({
+        next: z.boolean(),
+        prev: z.boolean()
+      })
+    ])
+    .default(true)
+    .transform(v => (typeof v === 'boolean' ? { next: v, prev: v } : v)),
+  nextThemes: z
+    .strictObject({
+      attribute: z
+        .union([attributeSchema, z.array(attributeSchema)])
+        .default('class'),
+      defaultTheme: z.string().optional(),
+      disableTransitionOnChange: z.boolean().default(true),
+      forcedTheme: z.string().optional(),
+      storageKey: z.string().optional()
+    })
+    .default({}),
+  pageMap: z.array(z.any({})),
+  search: reactNode.default(<Search />),
+  sidebar: z
+    .strictObject({
+      autoCollapse: z.boolean().optional(),
+      defaultMenuCollapseLevel: z.number().min(1).int().default(2),
+      defaultOpen: z.boolean().default(true),
+      toggleButton: z.boolean().default(true)
+    })
+    .default({}),
+  themeSwitch: z
+    .strictObject({
+      dark: z.string().default('Dark'),
+      light: z.string().default('Light'),
+      system: z.string().default('System')
+    })
+    .default({}),
+  toc: z
+    .strictObject({
+      backToTop: reactNode.default('Scroll to top'),
+      extraContent: reactNode,
+      float: z.boolean().default(true),
+      title: reactNode.default('On This Page')
+    })
+    .default({})
+})
+
+export type ThemeConfigProps = z.infer<typeof theme>
+
+type LayoutProps = z.input<typeof theme> & { children: ReactNode }
+
+export const Layout: FC<LayoutProps> = ({ children, ...themeConfig }) => {
+  const { data, error } = theme.safeParse(themeConfig)
+  if (error) {
+    throw fromZodError(error)
+  }
+  const { footer, navbar, pageMap, nextThemes, banner, ...rest } = data
+
+  return (
+    <ThemeConfigProvider value={rest}>
+      <ThemeProvider {...nextThemes}>
+        <SkipNavLink />
+        {banner}
+        <ConfigProvider pageMap={pageMap} navbar={navbar} footer={footer}>
+          {/*
+           * MobileNav should be in layout and not in mdx wrapper, otherwise for non mdx pages will
+           * be not rendered
+           */}
+          <MobileNav />
+          {children}
+        </ConfigProvider>
+      </ThemeProvider>
+    </ThemeConfigProvider>
+  )
+}
